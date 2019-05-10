455cd6ba55ebfe16043f9ea08e3f5372410bc2ec
reginaldolauri <reginaldo.lauri@gmail.com>
2019-05-10 12:30:06 -0300

    Continuação da aula 10/05/2019.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,32 @@
 class NegociacaoController{
 
-    adiciona(event){
-        event.preventDefault()
-        
+    constructor(){
         let $ = document.querySelector.bind(document);
         
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
 
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+    adiciona(event){
+        event.preventDefault()
+        let listaNegociacoes = new ListaNegociacoes();
+        listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade = 0;
+        this._inputValor = 0.0;
+        this._inputData.focus();
     }
 }
---
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,16 @@
+class DateHelper {
+
+    constructor(){
+        throw new Error('Essa classe não pode ser instanciada.');
+    }
+
+    static textoParaData(texto){
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('A data informada deve estar no formato aaaa-mm-dd.')
+        return new Date(...texto.split('-').map((data, indice) => data - indice % 2));
+    }
+
+    static dataParaTexto(data){
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    }
+}
