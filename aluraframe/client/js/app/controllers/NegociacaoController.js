4fcd09e86ce8ed35187a4760a9c941467c2dda57
reginaldolauri <reginaldo.lauri@gmail.com>
2019-05-27 15:14:55 -0300

    Continuação javascript avançado - ProxyFactory

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,13 +6,22 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        
+        let self = this;
+        
+        this._listaNegociacoes = ProxyFactory.create(
+            new ListaNegociacoes(),
+            ["adiciona", "esvazia"], 
+            model => this._negociacaoView.update(model));
+        
         this._negociacaoView = new NegociacaoView($('#negociacoesView'));
-       
-        /*this._listaNegociacoes = new ListaNegociacoes(model =>
-            this._negociacaoView.update(model));*/
-       
         this._negociacaoView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
+        
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ["texto"],
+            model => this._mensagemView.update(model));
+
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
     }
@@ -20,17 +29,13 @@ class NegociacaoController{
     apaga(){
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso.'
-        this._mensagemView.update(this._mensagem);
-        
         this._limpaFormulario();
     }
 
     adiciona(event){
         event.preventDefault()
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        
         this._mensagem.texto = 'Negociação incluída com sucesso.'
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
 
