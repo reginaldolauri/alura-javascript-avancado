25e62be6f36f85756fbacfe869a3732e750b596f
reginaldolauri <reginaldo.lauri@gmail.com>
2019-06-18 14:04:00 -0300

    Implementação do DAO no Controller.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -19,6 +19,14 @@ class NegociacaoController{
             'texto');
         
         this._ordemAtual = '';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao => 
+                    this._listaNegociacoes.adiciona(negociacao)))
     }
 
     apaga(){
@@ -28,21 +36,30 @@ class NegociacaoController{
     }
 
     adiciona(event){
-        event.preventDefault()
-        try {
-            this._listaNegociacoes.adiciona(this._criaNegociacao());
-            this._mensagem.texto = 'Negociação incluída com sucesso.'
-            this._limpaFormulario();
-        } catch (erro) {
-            this._mensagem.texto = erro;
-        }
+        event.preventDefault();
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => {
+                
+                let negociacao = this._criaNegociacao();
+                
+                new NegociacaoDao(connection)
+                    .adiciona(negociacao)
+                    .then(() => {
+                        this._listaNegociacoes.adiciona(negociacao);
+                        this._mensagem.texto = 'Negociação adicionada com sucesso.';
+                        this._limpaFormulario();
+                    })
+            })
+            .catch(erro => this._mensagem.texto = erro);
     }
 
     _criaNegociacao(){
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
         );
     }
 
