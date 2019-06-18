e7ceb0e7567caa2a35b63c419b11f1a472b5a89e
reginaldolauri <reginaldo.lauri@gmail.com>
2019-06-18 10:39:46 -0300

    Implementação completa de ConnectionFactory

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,29 +1,58 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraFrame'
-class ConnectionFactory {
+var ConnectionFactory = (function (){
 
-    constructor(){
-        throw new Error('Não é possível criar instâncias de ConnectionFactory')
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe'
+    
+    var connection = null;
+    
+    var close = null;
+    return class ConnectionFactory {
+    
+        constructor(){
+            throw new Error('Não é possível criar instâncias de ConnectionFactory')
+        }
+    
+        static getConnection(){
+            return new Promise((resolve, reject) => {
+    
+                let openRequest = window.indexedDB.open(dbName, version);
+    
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+    
+                openRequest.onsuccess = e => {
+                    if (!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function(){
+                            throw new Error('Você não pode fechar diretamente a conexão.')
+                        };
+                    }
+                    resolve(connection);
+                };
+    
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
+            });
+        }
+    
+        static _createStores(connection){
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.createObjectStore(store, {autoIncrement: true});
+                }
+            });        
+        }
+
+        static closeConnection(){
+            if (connection) {
+                close();
+                connection = null;
+            }
+        }
     }
-
-
-    static getConnection(){
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName, version);
-
-            openRequest.onupgradeneeded = e => {
-
-            };
-
-            openRequest.onsuccess = e => {
-
-            };
-
-            openRequest.onerror = e => {
-
-            };
-        });
-    }
-}
+})();
